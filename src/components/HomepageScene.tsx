362639b0759943db4745ec24ac2f1f7854c092e2
Thijnmens <thijnsmulders04@gmail.com>
2024-06-04 15:45:31 +0200

    Add smoothness to eye movement

--- src/components/HomepageScene.tsx
+++ src/components/HomepageScene.tsx
@@ -7,14 +7,14 @@ export default function HomepageScene() {
 	const self = useRef<HTMLDivElement>(null);
 	const [mouseLocation, setMouseLocation] = useState<number[]>([0, 0]);
 
-	function getDirection(e: MouseEvent) {
+	function calculateMouseLocation(e: MouseEvent) {
 		const mouseX = -10 + (20 / (self.current?.offsetWidth ?? window.innerWidth)) * e.clientX;
 		const mouseY = (-10 + (20 / (self.current?.offsetHeight ?? 11)) * e.clientY) * -1;
 		setMouseLocation([mouseX, mouseY]);
 	}
 
 	return (
-		<div ref={self} className="h-screen w-full" onMouseMove={getDirection}>
+		<div ref={self} className="h-screen w-full" onMouseMove={calculateMouseLocation}>
 			<Canvas>
 				<ambientLight intensity={Math.PI / 2} />
 				<spotLight position={[69, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
@@ -27,16 +27,22 @@ export default function HomepageScene() {
 }
 
 const Eye = (props: { mouseX: number; mouseY: number; location: [x: number, y: number, z: number] }) => {
-	const rotationRef = useRef<Object3D>(null);
+	const sphereRef = useRef<Object3D>(null);
+	const lerpRef = useRef<Object3D>(null);
 
 	useFrame(() => {
-		if (!rotationRef.current) return;
-		rotationRef.current.lookAt(new Vector3(props.mouseX, props.mouseY, 5));
+		if (!sphereRef.current || !lerpRef.current) return;
+		lerpRef.current.lookAt(new Vector3(props.mouseX, props.mouseY, 5));
+		sphereRef.current.quaternion.slerpQuaternions(sphereRef.current.quaternion, lerpRef.current.quaternion, 0.05);
+		sphereRef.current.position.set(...props.location);
 	});
 
 	return (
-		<object3D ref={rotationRef} position={props.location}>
-			<Sphere texture={'../assets/Eye_4096.png'} />
-		</object3D>
+		<>
+			<object3D ref={lerpRef} />
+			<object3D ref={sphereRef}>
+				<Sphere texture={'../assets/Eye_4096.png'} />
+			</object3D>
+		</>
 	);
 };
