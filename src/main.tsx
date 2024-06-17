6b16648c41ffe301884d182d33d24529290c2b72
Thijnmens <thijnsmulders04@gmail.com>
2024-06-17 15:40:21 +0200

    Rename pages to routes

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import App from './App.tsx';
 import './index.css';
 import Navbar from './components/Navbar.tsx';
 import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-dom';
-import Projects from './pages/Projects.tsx';
+import Projects from './routes/Projects.tsx';
+import ErrorPage from './routes/ErrorPage.tsx';
 
 export default function pages(): RouteObject[] {
 	return [
