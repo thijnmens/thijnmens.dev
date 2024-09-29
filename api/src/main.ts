f7abf8f0e9633789ab9cd6ec40759f7467d3f8a7
Thijnmens <thijnsmulders04@gmail.com>
2024-09-29 13:33:25 +0000

    Update scripts

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,34 +5,35 @@ import compression from 'compression';
 import rateLimit from 'express-rate-limit';
 import dotenv from 'dotenv';
 
-console.log(`.env.${process.env.NODE_ENV}`)
-
 // Load ENV
 dotenv.config({
-	path: `.env.${process.env.NODE_ENV}`
-})
+	path: `.env.${process.env.NODE_ENV}`,
+});
 
 // Configure express
 const app = express();
 
-app.use(cors({
-	origin: process.env.API_URL as string,
-}))
-app.use(helmet())
-app.use(compression())
-app.use(rateLimit({
-	windowMs: 1 * 60 * 1000, // 1 Minute
-	limit: 20
-}))
+app.use(
+	cors({
+		origin: process.env.API_URL as string,
+	}),
+);
+app.use(helmet());
+app.use(compression());
+app.use(
+	rateLimit({
+		windowMs: 1 * 60 * 1000, // 1 Minute
+		limit: 20,
+	}),
+);
 
 // Serve static content
 app.use(express.static(process.env.STATIC_DIR as string));
 
-
-app.get("/", (_, res) => {
-	res.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-})
+app.get('/', (_, res) => {
+	res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+});
 
 app.listen(process.env.API_PORT, () => {
-	console.log(`API running on ${process.env.API_PORT}`)
-})
+	console.log(`API running on ${process.env.API_PORT} in ${process.env.NODE_ENV} mode`);
+});
